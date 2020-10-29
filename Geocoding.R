1863a407090b6a8ae5da5cbd1079c5641e3a4e89
Pablo Aguirre Hörmann <47273633+pjaguirreh@users.noreply.github.com>
2020-10-29 10:54:40 -0300

    Update Geocoding.R

--- Geocoding.R
+++ Geocoding.R
@@ -6,7 +6,6 @@ library(tidyverse)
 library(ggmap)
 library(sf)
 library(mapview)
-library(leaflet)
 
 datos_mesa <- read_csv("DatosPlebiscitoMesa.csv")
 
@@ -20,17 +19,17 @@ register_google(key = "PONER API DE GOOGLE", write = TRUE)
 
 locales_direccion <- mutate_geocode(locales, location = paragoogle, output = "latlon")
 
-datos_RM_datos_mesa <- datos_mesa %>% 
+datos_RM_mesa_coord <- datos_mesa %>% 
   mutate(paragoogle = paste(Comuna, Local, sep = "+"),
          paragoogle = str_remove_all(paragoogle, '\\"')) %>%
   filter(Region == "METROPOLITANA SANTIAGO") %>% 
   left_join(select(locales_direccion, paragoogle, lon, lat), by = "paragoogle") %>% 
   filter(!is.na(lon))
 
-datos_RM_datos_mesa %>% 
-  leaflet() %>% 
-  addTiles() %>%
-  addMarkers(~lon, ~lat, popup = ~as.character(Comuna), label = ~as.character(Apruebo))
+datos_RM_mesa_coord %>% 
+  write_excel_csv("DatosPlebiscitoMesaRMCoord.csv")
+
+mapa_sf <- st_as_sf(datos_RM_mesa_coord, coords = c("lon", "lat"), crs = 4326)
 
 mapview(mapa_sf)
 
